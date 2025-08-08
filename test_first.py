d579d8f7aafa3e0e0f809c4ca2ba837660622d57
Ivan Zhelev <ivanzhelev@Ivans-MacBook-Air.local>
2025-08-08 17:20:37 +0300

    Added Day 1 Playwright script to open Chromium page and go to Playwright dev page then close the browser with a captured screenshot

---
+++ test_first.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import sync_playwright
+
+def run():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://playwright.dev/")
+        page.screenshot(path="screenshots/playwright_home.png")
+        browser.close()
+
+if __name__ == "__main__" :
+    run()
